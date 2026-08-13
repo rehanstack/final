3c26ef7a5b6a184ca0d120a6200183c52b0cc717
rehaansk777-boop <rehaan.sk777@gmail.com>
2026-08-13 15:37:36 +0530

    url udpdated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://final-backend-topaz.vercel.app/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
